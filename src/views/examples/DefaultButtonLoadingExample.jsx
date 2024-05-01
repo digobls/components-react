45ee5b0100fcef5a929b06945a1f9c7e6efc7608
Rodrigo_Pereira <digo.bls@hotmail.com>
2024-04-30 23:10:19 -0300

    fix: add config button loading

--- src/views/examples/DefaultButtonLoadingExample.jsx
+++ src/views/examples/DefaultButtonLoadingExample.jsx
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import DefaultButtonLoading from '../../components/defaultButtonLoading/DefaultButtonLoading';
+import DefaultInput from '../../components/defaultInputTypes/DefaultInput';
+import {useForm} from 'react-hook-form';
 
 function SkeletonLoadingExample() {
-    const [loadingSend, setLoadingSend] = useState(false);
+    const {
+        register: formButton,
+        setValue: setFormButton,
+        watch: watchFormButton,
+    } = useForm({defaultValues: {text: 'Salvar', loading: false, disable: false}}); // Set formState mode to 'all' for immediate validation
+
     const handleButtonClick = () => {
         console.log('clickButton');
-        setLoadingSend(true);
-        setTimeout( () => {
-            setLoadingSend(false);
+        setFormButton('loading', true);
+        setTimeout(() => {
+            setFormButton('loading', false);
         }, 2000);
     }
 
@@ -21,17 +28,51 @@ function SkeletonLoadingExample() {
                     </div>
                 </div>
 
-                <div className="col-12">
+                <div className="row">
+                    <div className="col-2">
+                        <DefaultInput
+                            id={'text'}
+                            type={'text'}
+                            label={'Texto botão'}
+                            placeholder={'Texto botão'}
+                            disable={false}
+                            isRequired={false}
+                            register={formButton}>
+                        </DefaultInput>
+                    </div>
+                    <div className="col-12"></div>
+                    <div className="col-3">
+                        <DefaultInput
+                            id={'loading'}
+                            type={'checkbox'}
+                            labelCheckbox={'Loading'}
+                            displayInline={true}
+                            setValue={setFormButton}
+                            register={formButton}
+                            watch={watchFormButton}>
+                        </DefaultInput>
+                    </div>
+                    <div className="col-3">
+                        <DefaultInput
+                            id={'disable'}
+                            type={'checkbox'}
+                            labelCheckbox={'Desabilitar botão'}
+                            displayInline={true}
+                            setValue={setFormButton}
+                            register={formButton}
+                            watch={watchFormButton}>
+                        </DefaultInput>
+                    </div>
+                </div>
+
+                <div className="col-12 offset-top-30">
                     <DefaultButtonLoading
                         id={'id'}
                         type={'button'}
-                        text={'Salvar'}
-                        showIcon={false}
-                        iconRight={false}
-                        disabled={false}
-                        loading={loadingSend}
-                        handleButtonClick={handleButtonClick}
-                    />
+                        text={watchFormButton('text')}
+                        disabled={watchFormButton('disable')}
+                        loading={watchFormButton('loading')}
+                        handleButtonClick={handleButtonClick}/>
                 </div>
             </div>
         </div>
